4feabcc399781a85adc2b95a8a3efb043345407e
21120612 <nmt6465@gmail.com>
2024-03-10 14:28:39 +0700

    create function global error Handdler

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,5 +1,6 @@
+import { wrapRequestHandler } from './../utils/handlers';
 import { validate } from './../utils/validation';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { ParamsDictionary } from 'express-serve-static-core';
 import User from '~/models/schemas/User.schema';
 import databaseService from '~/services/db.services';
@@ -16,26 +17,24 @@ export const loginController = (req: Request, res: Response) => {
   }
 };
 
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
-  const { email, password } = req.body;
-  if (!email && !password) {
-    res.status(401).json({ message: 'Email and password is required' });
-  } else {
-    try {
+export const registerController = wrapRequestHandler(
+  async (
+    req: Request<ParamsDictionary, any, RegisterReqBody>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { email, password } = req.body;
+    if (!email && !password) {
+      res.status(401).json({ message: 'Email and password is required' });
+    } else {
       const result = await userService.register(req.body);
       return res.status(200).json({
         message: 'Register successfully',
         data: result
       });
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json({
-        message: 'Register failed',
-        error: err
-      });
     }
   }
-};
+);
 
 export const registerValidator = validate(
   checkSchema({
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const app = express();
 DatabaseService.run().catch(console.dir);
 app.use(express.json());
 app.use('/api/v1/users', userRouter);
+app.use('*', (err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
