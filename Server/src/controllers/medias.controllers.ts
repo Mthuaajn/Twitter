5b8cd311d88725b1d32e233caef4a7845eb475e4
21120612 <nmt6465@gmail.com>
2024-04-21 23:34:34 +0700

    filter file image

--- Server/src/controllers/medias.controllers.ts
+++ Server/src/controllers/medias.controllers.ts
@@ -1,19 +1,12 @@
 import { Request, Response } from 'express';
+import { result } from 'lodash';
 import path from 'path';
+import HTTP_STATUS from '~/constants/httpStatus';
+import { handleFileUploadImageSingle } from '~/utils/file';
+
 export const uploadSingleImageController = async (req: Request, res: Response) => {
-  const formidable = (await import('formidable')).default;
-  const form = formidable({
-    maxFiles: 1,
-    uploadDir: path.resolve('uploads'),
-    keepExtensions: true,
-    maxFileSize: 300 * 1024 // 300kb
-  });
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err;
-    }
-  });
-  res.json({
-    message: 'Upload file success'
+  const data = await handleFileUploadImageSingle(req);
+  return res.status(HTTP_STATUS.OK).json({
+    result: data
   });
 };
--- Server/src/utils/file.ts
+++ Server/src/utils/file.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express';
 import fs from 'fs';
 import path from 'path';
 export const initFileUpload = async () => {
@@ -6,4 +7,34 @@ export const initFileUpload = async () => {
     console.log('create file upload folder success');
     fs.mkdirSync(uploadFolderPath, { recursive: true });
   }
+};
+
+export const handleFileUploadImageSingle = async (req: Request) => {
+  const formidable = (await import('formidable')).default;
+  const form = formidable({
+    maxFiles: 1,
+    uploadDir: path.resolve('uploads'),
+    keepExtensions: true,
+    maxFileSize: 300 * 1024, // 300kb,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const isValid = name === 'image' && Boolean(mimetype?.includes('image/'));
+      if (!isValid) {
+        form.emit('err' as any, new Error('file type is not valid') as any);
+      }
+      return isValid;
+    }
+  });
+
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err);
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('file is empty'));
+      }
+      return resolve(files);
+    });
+  });
 };
