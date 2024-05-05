b029bbe2de500e2c2eb8131a5d756aa86a5d6a85
21120612 <nmt6465@gmail.com>
2024-05-06 00:31:04 +0700

    fix bug header content-Range no play video

--- Server/src/controllers/static.controllers.ts
+++ Server/src/controllers/static.controllers.ts
@@ -18,19 +18,42 @@ export const serveImageController = (req: Request, res: Response) => {
 
 export const serveVideoStreamController = async (req: Request, res: Response) => {
   const range = req.headers.range;
-  console.log('range', range);
   if (!range) {
     return res.status(HTTP_STATUS.BAD_REQUEST).send('Requires Range header');
   }
 
   const { name } = req.params;
   const videoPath = path.resolve(UPLOAD_VIDEO_DIR, name);
+  // Dung lượng video (bytes)
   const videoSize = fs.statSync(videoPath).size;
-  const chunkSize = 10 ** 6;
+  const chunkSize = 30 * 10 ** 6; // 30MB
+  // Lấy giá trị byte bắt đầu từ header Range (vd: bytes=1048576-)
   const start = Number(range.replace(/\D/g, ''));
-  const end = Math.min(start + chunkSize, videoSize);
-  const contentLength = end - start;
+  // Lấy giá trị byte kết thúc, vượt quá dung lượng video thì lấy giá trị videoSize - 1
+  const end = Math.min(start + chunkSize, videoSize - 1);
+  // Dung lượng thực tế cho mỗi đoạn video stream
+  // THường đây sẽ là chunkSize, ngoại trừ đoạn cuối cùng
+  const contentLength = end - start + 1;
   const contentType = (await import('mime')).default.getType(videoPath) || 'video/*';
+  /**
+   * lúc đầu chưa sửa lại thì bị lỗi là do end = videoSize nên bị lỗi không thể load lại được video
+   * Format của header Content-Range: bytes <start>-<end>/<videoSize>
+   * Ví dụ: Content-Range: bytes 1048576-3145727/3145728
+   * Yêu cầu là `end` phải luôn luôn nhỏ hơn `videoSize`
+   * ❌ 'Content-Range': 'bytes 0-100/100'
+   * ✅ 'Content-Range': 'bytes 0-99/100'
+   *
+   * Còn Content-Length sẽ là end - start + 1. Đại diện cho khoản cách.
+   * Để dễ hình dung, mọi người tưởng tượng từ số 0 đến số 10 thì ta có 11 số.
+   * byte cũng tương tự, nếu start = 0, end = 10 thì ta có 11 byte.
+   * Công thức là end - start + 1
+   *
+   * ChunkSize = 50
+   * videoSize = 100
+   * |0----------------50|51----------------99|100 (end)
+   * stream 1: start = 0, end = 50, contentLength = 51
+   * stream 2: start = 51, end = 99, contentLength = 49
+   */
   const headers = {
     'Content-Range': `bytes ${start}-${end}/${videoSize}`,
     'Accept-Ranges': 'bytes',
