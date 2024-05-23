b174c26de7d7efc4e74dd98cf0e500699c4807c8
21120612 <nmt6465@gmail.com>
2024-05-23 09:04:21 +0700

    remove tweet_children in tweet_detail with

--- Server/src/middlewares/tweet.middlewares.ts
+++ Server/src/middlewares/tweet.middlewares.ts
@@ -222,6 +222,11 @@ export const tweetIdValidator = validate(
                       }
                     }
                   }
+                },
+                {
+                  $project: {
+                    tweet_children: 0
+                  }
                 }
               ])
               .toArray();
