6a9998ef7f95af28136b391d7a50730d32b9d4e5
21120612 <nmt6465@gmail.com>
2024-06-11 20:27:10 +0700

    connect s3 va console list buckets creating

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -14,6 +14,7 @@ import likeRouter from './routes/like.routes';
 import searchRouter from './routes/search.routes';
 import morgan from 'morgan';
 import cors from 'cors';
+import '~/utils/S3';
 // import '~/utils/fake';
 config();
 export class App {
