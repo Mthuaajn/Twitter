e351ee4e37d7cca8ee4c9eb6667f837b50c992e5
21120612 <nmt6465@gmail.com>
2024-06-11 21:15:31 +0700

    test upload image

--- Server/src/utils/S3.ts
+++ Server/src/utils/S3.ts
@@ -1,5 +1,8 @@
-import { S3 } from '@aws-sdk/client-s3';
+import { S3, S3Client } from '@aws-sdk/client-s3';
 import { config } from 'dotenv';
+import { Upload } from '@aws-sdk/lib-storage';
+import fs from 'fs';
+import path from 'path';
 config();
 
 const s3 = new S3({
@@ -9,4 +12,38 @@ const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 });
-s3.listBuckets({}).then((data) => console.log(data));
+
+const file = fs.readFileSync(path.resolve('uploads/images/5f9d9862ae61c911bf7d1d300.jpg'));
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: { Bucket: 'tweet-clone-2023', Key: 'anh1.jpg', Body: file, ContentType: 'image/jpg' }, // bucket la cai bucket tao tren aws
+  // key la ten file upload
+  // body thi chon buffer la file minh upload len
+  // optional tags
+  // bo sung contentType de khong bi download anh ve ma co the xem tren trinh duyet
+
+  tags: [
+    /*...*/
+  ],
+
+  // additional optional fields show default values below:
+
+  // (optional) concurrency configuration
+  queueSize: 4,
+
+  // (optional) size of each part, in bytes, at least 5MB
+  partSize: 1024 * 1024 * 5,
+
+  // (optional) when true, do not automatically call AbortMultipartUpload when
+  // a multipart upload fails to complete. You should then manually handle
+  // the leftover parts.
+  leavePartsOnError: false
+});
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress);
+});
+
+parallelUploads3.done().then((res) => {
+  console.log(res);
+});
